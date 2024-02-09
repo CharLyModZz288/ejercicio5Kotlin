97f9bae33ae18c8f7ff936ed86fde015d4bcce0f
alumno1 <caesse280801@gmail.com>
2024-02-09 11:27:18 +0100

    ejercicio 1 hecho

---
+++ app/src/main/java/com/example/ejercicio5_kottlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.ejercicio5_kottlin
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var container: RelativeLayout
+    lateinit var editTextMensaje: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun cambiarColorFondo(view: View) {
+        container.setBackgroundColor(Color.BLUE)
+    }
+    fun abrirActividad2(view: View) {
+        val mensaje = editTextMensaje.text.toString() // Obtenemos el texto del EditText
+        val intent = Intent(this, Activity2::class.java)
+        intent.putExtra("mensaje", mensaje) // Enviamos el mensaje como extra en la Intent
+        startActivity(intent)
+    }
+}
